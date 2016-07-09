70fae5dd0c7596194f7fd41f04ece5acab4b02ef
Tyler Barton <tbarton33@outlook.com>
2016-07-09 17:37:38 -0500

    This is the first version of the voice recognition program, following Johnny Manson's tutorial at https://www.youtube.com/watch?v=AB9lfHDOe5U

---
+++ VoiceControlApplication2/frmVoiceControl.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace VoiceControlApplication2
+{
+    partial class frmVoiceControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVoiceControl));
+            this.rtbVoiceLog = new System.Windows.Forms.RichTextBox();
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbVoiceLog
+            // 
+            resources.ApplyResources(this.rtbVoiceLog, "rtbVoiceLog");
+            this.rtbVoiceLog.Name = "rtbVoiceLog";
+            this.rtbVoiceLog.TextChanged += new System.EventHandler(this.rtbVoiceLog_TextChanged);
+            // 
+            // btnEnable
+            // 
+            resources.ApplyResources(this.btnEnable, "btnEnable");
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            resources.ApplyResources(this.btnDisable, "btnDisable");
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // frmVoiceControl
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Controls.Add(this.rtbVoiceLog);
+            this.Name = "frmVoiceControl";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbVoiceLog;
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+    }
+}
+
---
+++ VoiceControlApplication2/frmVoiceControl.cs
@@ -0,0 +1,75 @@
+﻿//This code references Johnny Manson's YouTube video (C# Development Tutorial | Voice Recognition):
+//https://www.youtube.com/watch?v=AB9lfHDOe5U
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+//using System.Speech.Synthesis;
+
+namespace VoiceControlApplication2
+{
+    public partial class frmVoiceControl : Form
+    {
+        public frmVoiceControl()
+        {
+            InitializeComponent();
+        }
+
+        SpeechRecognitionEngine speechRecEngine = new SpeechRecognitionEngine();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices voiceCommands = new Choices();
+            voiceCommands.Add(new string[] { "Say hello.", "Print my name."});
+            GrammarBuilder gramBuilder = new GrammarBuilder();
+            gramBuilder.Append(voiceCommands);
+            Grammar grammer = new Grammar(gramBuilder);
+
+            //speechRecEngine.LoadGrammar
+            //speechRecEngine.AsyncGrammar(grammer);
+            speechRecEngine.LoadGrammarAsync(grammer);
+            speechRecEngine.SetInputToDefaultAudioDevice();
+            speechRecEngine.SpeechRecognized += speechRecEngine_SpeechRecognized;
+        }
+
+        void speechRecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "Say hello.":
+                    MessageBox.Show("Hello Tyler.  How are you?");
+                    break;
+                case "Print my name.":
+                    rtbVoiceLog.Text += "\nTyler";
+                    break;
+            }
+        }
+
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            speechRecEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+            btnEnable.Enabled = false;
+        }
+
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            speechRecEngine.RecognizeAsyncStop();
+            btnEnable.Enabled = true;
+            btnDisable.Enabled = false;
+        }
+
+        private void rtbVoiceLog_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
